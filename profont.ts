b661128f04c2e5a4b98fd8f60c106f4652ea2a46
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-01-13 06:59:34 +0900

    Updates.

--- profont.ts
+++ profont.ts
@@ -1,18 +1,33 @@
 /**
  * Propotional Font Display blocks
  */
-enum rotate{
+enum rotate {
     //% block="top"
-    top =0,
+    top = 0,
     //% block="left"
-    left=1,
+    left = 1,
     //% block="under"
-    under=2,
+    under = 2,
     //% block="right"
-    right=3
+    right = 3
 }
 //% weight=100 color=#0fbc11 icon="\u270f" block="Proportional Font"
 namespace profont {
+    //% shim=profont::getAlph
+    function getAlph(n: number): number {
+        return 0;
+    }
+
+    //% shim=profont::getKana
+    function getKana(n: number): number {
+        return 0;
+    }
+
+    //% shim=profont::getKanji
+    function getKanji(n: number): number {
+        return 0;
+    }
+
     declare const enum mojiSHift {
         Alfa = 0,
         Kana = 1,
@@ -22,10 +37,14 @@ namespace profont {
     let scroleSpeed: number = 200
     let yose: number = -1       //-1:left,0:center,1:right
     let kanaShift: number = 0	//0:Alfa,1:Kana,2:Kanji
-    function getFont(charCode:number):string{
+    function getFont(charCode: number): string {
         const AlfaFont: string = "0    M    H0H  :O:O::MEG:IB49C:EE:1H    >A   A>   :4:  4>4  12   44   2    248  >A>  9O1  CE9  AEK  L4O  MEB  >E2  @CL  :E:  9E>  :    1:   4:A  ::   A:4  @E8  >AEB>?D?  OE:  >AA  OA>  OEA  OD@  >E6  O4O  AOA  2AN  O<C  O11  O8O  O>O  OAO  OD8  >C?  OD;  9EB  @O@  O1O  N1N  O2O  K4K  H7H  CEI  OA   842  AO   8@8  111  @8   7:7  O52  699  25O  6=5  4?D  9E:  O43  ;    11F  O25  N11  ?4?  ?87  696  ?:4  4:?  788  5=:  4?5  >1?  >1>  ?2?  969  =3<  ;=9  4OA  O    AO4  4848 0    "
         const KanaFont: string = "0    252  N@@  11?  21   04   @EEN 8?:< 1278 4=6  575  56?4 4?46 5571 EEO  4524 444  AFDH 44?@ <I>  AOA  9:O8 9N9> :O:  8ABL 4I>8 AAAO 8M9N8EE2L ABDJA8N9= @92L 9IFL 5END HI1N 5EF4 O42  99N8 1AA1 AEBM 9:K:=112L 1N0L3N999 AABL 6842 ;8O8;DBEH EEE1 3=A531:4J DOEE 8O:< AAO1 EEEO EEEF L11N 1N0O2O124 OAAO HAAN AA2L HH   8D8  "
         const KanjiFont: string = "=G4G8OEO  1NDO 92L2956O:A9:O:95;O;519O91"
+
+        let retNum: number
+        retNum = getAlph(0x21);
+
         if ((charCode >= 0x20) && (charCode <= 0x5f))
             return trim(AlfaFont.substr((charCode - 0x20) * 5, 5));
         else if ((kanaShift == mojiSHift.Alfa) && (charCode >= 0x60) && (charCode <= 0x7e))
